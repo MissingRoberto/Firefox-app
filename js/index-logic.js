ccf6d4d263684ce4d2b92bfac6602f802091255c
Roberto Jiménez Sánchez <totemteleko@gmail.com>
2013-05-10 16:22:07 +0200

    commit

---
+++ js/index-logic.js
@@ -0,0 +1,19 @@
+$(document).ready(){
+	
+
+
+$('#ok_button').click( function(){
+	
+		});
+$('#problem_button').click( function(){
+
+//var sms = navigator.mozSms;
+// Send a message
+
+//sms.send("123456789", "Hello world!");
+	
+		});
+
+
+
+}
---
+++ js/logic.js
@@ -0,0 +1,11 @@
+$(document).ready(){
+	$('#add_button').click( function(){
+
+		//var json = { 'name': $("#name")[0].value, 'amount': $("#amount")[0].value, 'date': $("#name")[0].value };
+
+		//localStorage.setItem('json', JSON.stringify(testObject));
+		});
+
+
+
+}
